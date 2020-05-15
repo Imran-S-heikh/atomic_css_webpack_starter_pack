35dd120a6edd8672fd63e25846c409898cb6aa17
Imran-S-heikh <52422304+Imran-S-heikh@users.noreply.github.com>
2020-05-15 13:59:50 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ npm init
 sed -i "/main/a\\ \"watch\":{\"cp\":\"src/style/molecules.js\"},"  package.json
 sed -i '/scripts/a\\ "start":"webpack-dev-server","build":"webpack","watch":"npm-watch","cp" : "cp src/style/molecules.js node_modules/atomizer/src/helpers.js"'  package.json
 sed -i '/"test"/d' package.json
-npm install -D babel-loader @babel/core @babel/preset-env webpack webpack-cli css-loader html-loader html-webpack-plugin mini-css-extract-plugin webpack-dev-server sass-loader node-sass file-loader autoprefixer postcss-loader
+npm install -D babel-loader @babel/core @babel/preset-env webpack webpack-cli css-loader style-loader webpack-atomizer-loader html-loader html-webpack-plugin webpack-dev-server file-loader autoprefixer postcss-loader
 #gnome-terminal -- npm start
 #rm init.sh
